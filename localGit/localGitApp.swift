c571bcb4037c24328f62530395e1c7c6d320070c
乔杨珺97222 <97222@sangfor.com>
2023-04-06 11:19:35 +0800

    Initial Commit

---
+++ localGit/localGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  localGitApp.swift
+//  localGit
+//
+//  Created by sangfor on 2023/4/6.
+//
+
+import SwiftUI
+
+@main
+struct localGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
