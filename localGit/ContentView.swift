ef8b7cd0815694bef440aa0decee047d92d8e54a
乔杨珺97222 <97222@sangfor.com>
2023-04-06 12:00:07 +0800

    commit test 2

--- localGit/ContentView.swift
+++ localGit/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("Hello, uem!")
+            Text("Hello, sangfor!")
         }
         .padding()
     }
